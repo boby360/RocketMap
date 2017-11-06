d2b090e6f1d9f21b53514fb45ee8a270a1c672f1
sLoPPydrive <sloppydrive@gmail.com>
2017-11-06 11:26:37 +0100

    Gym icons are now generated on demand using ImageMagick commands
    Note that the ImageMagick "convert" command must be present in your path and you need the special sprites pack from Discord.

---
+++ pogom/dyn_img.py
@@ -0,0 +1,71 @@
+import os
+import subprocess
+
+import logging
+from string import join
+
+log = logging.getLogger(__name__)
+
+out_dir = "static/images/generated"
+target_size = 96    # Width/height of target gym icon in pixels
+
+
+def get_gym_icon(team, level, raidlevel, pkm):
+    init_image_dir()
+    level = int(level)
+
+    subject_lines = []
+    badge_lines = []
+    white_transparent = "\"rgba(255, 255, 255, 0.7)\""
+    black_transparent = "\"rgba(0, 0, 0, 0.7)\""
+    if pkm:
+        out_filename = "{}/{}_{}_{}.png".format(out_dir, team, level, pkm)
+        subject_lines = draw_subject("static/icons/{}.png".format(pkm), float(2) / 3)
+        badge_lines.extend(draw_badge(75, 20, 15, white_transparent, "black", raidlevel))
+        if level > 0:
+            badge_lines.extend(draw_badge(75, 76, 15, black_transparent, "white", level))
+    elif raidlevel:
+        raidlevel = int(raidlevel)
+        egg_name = "legendary" if raidlevel == 5 else ("rare" if raidlevel > 2 else "normal")
+        out_filename = "{}/{}_{}_{}.png".format(out_dir, team, level, egg_name)
+        subject_lines = draw_subject("static/images/raid/egg_{}.png".format(egg_name), 0.5)
+        badge_lines.extend(draw_badge(75, 20, 15, white_transparent, "black", raidlevel))
+        if level > 0:
+            badge_lines.extend(draw_badge(75, 76, 15, black_transparent, "white", level))
+    elif level > 0:
+        out_filename = "{}/{}_{}.png".format(out_dir, team, level)
+        badge_lines.extend(draw_badge(75, 76, 15, black_transparent, "white", level))
+    else:
+        return "static/images/gym/{}.png".format(team)
+
+    if not os.path.isfile(out_filename):
+        gym_image = "static/images/gym/{}.png".format(team)
+        cmd = "convert {} {} -gravity center -font ArialBk -pointsize 25 {} {}".format(gym_image, join(subject_lines),
+                                                                                       join(badge_lines), out_filename)
+        subprocess.call(cmd, shell=True)
+    return out_filename
+
+
+def draw_subject(image, scale):
+    scaled_size = int(target_size * scale)
+    lines = []
+    lines.append(
+        "-gravity north \( {} -resize {}x{} \( +clone -background black -shadow 80x3+5+5 \) +swap -background none -layers merge +repage \) -geometry +0+0 -composite".format(
+            image, scaled_size, scaled_size))
+    return lines
+
+
+def draw_badge(x, y, r, fcol, tcol, text):
+    lines = []
+    lines.append('-fill {} -draw "circle {},{} {},{}"'.format(fcol, x, y, x+r, y))
+    lines.append('-fill {} -draw \'text {},{} "{}"\''.format(tcol, x-48, y-49, text))
+    return lines
+
+
+def init_image_dir():
+    if not os.path.isdir(out_dir):
+        try:
+            os.makedirs(out_dir)
+        except OSError as exc:
+            if not os.path.isdir(out_dir):
+                raise
